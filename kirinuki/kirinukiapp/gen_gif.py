93f5480646d118354a44d72da34d4bc75d7fc830
dddsuz2 <daidai121207@gmail.com>
2023-09-11 23:53:50 +0900

    commit

--- kirinuki/kirinukiapp/gen_gif.py
+++ kirinuki/kirinukiapp/gen_gif.py
@@ -7,9 +7,16 @@ from PIL import Image
 def convert_movie_to_jpgs(path):
     video_capture = cv2.VideoCapture(path)
     still_reading, image = video_capture.read()
+    print(video_capture.isOpened())
     frame_count = 0
-    while still_reading:
-        cv2.imwrite(f"output/frame_{frame_count:03d}.jpg", image)
-        
+    print(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = math.ceil(video_capture.get(cv2.CAP_PROP_FPS))
+
+    
+    while frame_count < fps * 6:
+        cv2.imwrite(f"{os.getcwd()}/kirinukiapp/output/frame_{frame_count:03d}.jpg", image)
+        a = Image.fromarray(image)
+        a.save(f"{os.getcwd()}/kirinukiapp/output/frame_{frame_count:03d}.jpg")
+        print(image.shape)
         still_reading, image = video_capture.read()
         frame_count += 1
