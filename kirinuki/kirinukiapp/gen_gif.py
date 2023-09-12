8bcfd91adb53e5016e41be7f2a65f356268fd564
dddsuz2 <daidai121207@gmail.com>
2023-09-12 23:57:40 +0900

    gen apng

--- kirinuki/kirinukiapp/gen_gif.py
+++ kirinuki/kirinukiapp/gen_gif.py
@@ -2,7 +2,10 @@ from itertools import count
 import math
 import os
 import cv2
+import glob
 from PIL import Image
+from moviepy.editor import VideoFileClip
+from apng import APNG
 
 def convert_movie_to_jpgs(path):
     video_capture = cv2.VideoCapture(path)
@@ -14,9 +17,25 @@ def convert_movie_to_jpgs(path):
 
     
     while frame_count < fps * 6:
-        cv2.imwrite(f"{os.getcwd()}/kirinukiapp/output/frame_{frame_count:03d}.jpg", image)
-        a = Image.fromarray(image)
-        a.save(f"{os.getcwd()}/kirinukiapp/output/frame_{frame_count:03d}.jpg")
+        cv2.imwrite(f"kirinuki/kirinukiapp/output/frame_{frame_count:03d}.jpg", image)
         print(image.shape)
         still_reading, image = video_capture.read()
         frame_count += 1
+
+
+def duration(file_path):
+    if file_path:
+        video = VideoFileClip(file_path)
+        video_duration = math.floor(video.duration)
+    return video_duration
+
+def gen_apng():
+    im = APNG()
+    path = 'kirinuki/kirinukiapp/output/*.jpg'
+    files = glob.glob(path)
+    for file in files:
+        im.append_file(file, delay=200)
+    im.save("kirinuki/kirinukiapp/result/result.png")
+    
+
+    
--- kirinuki/kirinukiapp/views.py
+++ kirinuki/kirinukiapp/views.py
@@ -4,16 +4,19 @@ from django.http import HttpResponseRedirect
 from .models import FileUpload
 from .forms import FileUploadForm
 from django.views.generic.edit import FormView
-from .gen_gif import convert_movie_to_jpgs
+from .gen_gif import convert_movie_to_jpgs, duration, gen_apng
+
 # Create your views here.
 
+file = None
 
-# ファイルアップロード
 def upload(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
+            global file
+            file = request.FILES['attach']
             return redirect('upload_complete')  # アップロード完了画面にリダイレクト
     else:
         form = FileUploadForm()
@@ -22,8 +25,10 @@ def upload(request):
 
 # ファイルアップロード完了
 def upload_complete(request):
-    uploaded_file = FileUpload.objects.first()
-    file_name = os.path.basename(uploaded_file.filename)
-    file_duration = int(uploaded_file.duration)
-    convert_movie_to_jpgs(uploaded_file.filename)
+    uploaded_file = file
+    file_name = uploaded_file.name
+    file_path = f"kirinuki/media/media/{file_name}"
+    file_duration = duration(file_path)
+    convert_movie_to_jpgs(file_path)
+    gen_apng()
     return render(request, 'kirinukiapp/upload_complete.html' ,{'file_name': file_name, 'file_duration': file_duration})
