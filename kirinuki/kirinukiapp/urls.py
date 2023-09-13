73026985eccf9e79675db463f01ed99c41d46ea8
dddsuz2 <daidai121207@gmail.com>
2023-09-13 23:24:34 +0900

    fetch API

--- kirinuki/kirinukiapp/urls.py
+++ kirinuki/kirinukiapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     #path("", views.Upload.upload_file, name="index"),
     path('upload/', views.upload, name='upload'),
     path('upload_complete/', views.upload_complete, name='upload_complete'),
+    path('generate_gif/', views.generate_image, name='generate_image'),
 ]
--- kirinuki/kirinukiapp/views.py
+++ kirinuki/kirinukiapp/views.py
@@ -1,6 +1,9 @@
 import os
+import cv2
+import json
+import base64
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
 from .models import FileUpload
 from .forms import FileUploadForm
 from django.views.generic.edit import FormView
@@ -30,5 +33,13 @@ def upload_complete(request):
     file_path = f"kirinuki/media/media/{file_name}"
     file_duration = duration(file_path)
     convert_movie_to_jpgs(file_path)
-    gen_apng()
     return render(request, 'kirinukiapp/upload_complete.html' ,{'file_name': file_name, 'file_duration': file_duration})
+
+# image生成
+def generate_image(request):
+    gen_apng()
+    _, encing = cv2.imencode("kirinuki/kirinukiapp/result/result.png")
+    img_str = encing.tostring()
+    img_byte = base64.b64encode(img_str).decode('utf-8')
+    img_json = json.dumps({'image': img_byte}).encode('utf-8')
+    return JsonResponse({"img_data": img_json})
